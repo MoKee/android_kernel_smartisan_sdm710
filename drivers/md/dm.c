301165b5bcaf243386608b8eb5c3c05bb853970c
XiNGRZ <chenxingyu92@gmail.com>
2019-03-12 13:17:41 +0800

    SMARTISAN: Import changes - device mapper
    Change-Id: I5f012c5e81a079a1173a6f3df72a3fcf05fdc989

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -880,6 +880,9 @@ static sector_t max_io_len(sector_t sector, struct dm_target *ti)
 {
 	sector_t len = max_io_len_target_boundary(sector, ti);
 	sector_t offset, max_len;
+#ifdef CONFIG_VENDOR_SMARTISAN
+	sector_t optimal_len;
+#endif
 
 	/*
 	 * Does the target need to split even further?
@@ -894,6 +897,11 @@ static sector_t max_io_len(sector_t sector, struct dm_target *ti)
 
 		if (len > max_len)
 			len = max_len;
+#ifdef CONFIG_VENDOR_SMARTISAN
+	} else if (ti->asymmetric_chunk_supported) {
+		ti->type->io_calculate(ti, sector, &optimal_len);
+		len = optimal_len;
+#endif
 	}
 
 	return len;
--- include/linux/device-mapper.h
+++ include/linux/device-mapper.h
@@ -118,6 +118,16 @@ typedef int (*dm_iterate_devices_fn) (struct dm_target *ti,
 typedef void (*dm_io_hints_fn) (struct dm_target *ti,
 				struct queue_limits *limits);
 
+#ifdef CONFIG_VENDOR_SMARTISAN
+/*
+ * This function will recalculate io size per bio if this target support
+ * asymmetric chunk size
+ */
+typedef void (*dm_io_len_calculate_fn) (struct dm_target *ti,
+				sector_t offset,
+				sector_t *optimal_len);
+#endif
+
 /*
  * Returns:
  *    0: The target can handle the next I/O immediately.
@@ -180,6 +190,9 @@ struct target_type {
 	dm_iterate_devices_fn iterate_devices;
 	dm_io_hints_fn io_hints;
 	dm_direct_access_fn direct_access;
+#ifdef CONFIG_VENDOR_SMARTISAN
+	dm_io_len_calculate_fn io_calculate;
+#endif
 
 	/* For internal device-mapper use. */
 	struct list_head list;
@@ -298,6 +311,13 @@ struct dm_target {
 	 * Set if this target does not return zeroes on discarded blocks.
 	 */
 	bool discard_zeroes_data_unsupported:1;
+
+#ifdef CONFIG_VENDOR_SMARTISAN
+	/*
+	 * Set if this target supported asymmetric chunk size.
+	 */
+	bool asymmetric_chunk_supported:1;
+#endif
 };
 
 /* Each target can link one of these into the table */
