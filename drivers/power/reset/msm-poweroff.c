kerneltoast/android_kernel_oneplus_msm8996

power: reset: msm: Always perform warm reset
To preserve console-ramoops across a reboot.

drivers/power/reset/msm-poweroff.c
@@ -292,6 +292,11 @@ static void msm_restart_prepare(const char *cmd)
 				(cmd != NULL && cmd[0] != '\0'));
 	}
 
+#ifdef CONFIG_MACH_MSM8996_15801
+	/* To preserve console-ramoops */
+	need_warm_reset = true;
+#endif
+
 	/* Hard reset the PMIC unless memory contents must be maintained. */
 	if (need_warm_reset) {
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);

MoKee/android_kernel_smartisan_sdm710

Change-Id: I0a46c700ebde24d8bd778bf71f076e6ae753631b

drivers/power/reset/msm-poweroff.c
@@ -306,6 +306,11 @@ static void msm_restart_prepare(const char *cmd)
 				(cmd != NULL && cmd[0] != '\0'));
 	}
 
+#ifdef CONFIG_VENDOR_SMARTISAN
+	/* To preserve console-ramoops */
+	need_warm_reset = true;
+#endif
+
 	/* Hard reset the PMIC unless memory contents must be maintained. */
 	if (need_warm_reset)
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);