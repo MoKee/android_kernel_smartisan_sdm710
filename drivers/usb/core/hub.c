8ec50522ee6c9965d3fb97b27776945b3d2d406c
XiNGRZ <chenxingyu92@gmail.com>
2019-07-30 10:36:54 +0800

    Revert "SMARTISAN: Import changes - USB"
    This reverts commit 0ed9ef835fbb670a9fc81d6d4b4f56b232ed2123.
    
    Change-Id: I491c429660aa21eb549ec9558da5ae99becad1ad

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4318,7 +4318,6 @@ static int hub_set_address(struct usb_device *udev, int devnum)
  * device says it supports the new USB 2.0 Link PM errata by setting the BESL
  * support bit in the BOS descriptor.
  */
-#ifndef CONFIG_VENDOR_SMARTISAN
 static void hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
 {
 	struct usb_hub *hub = usb_hub_to_struct_hub(udev->parent);
@@ -4336,7 +4335,6 @@ static void hub_set_initial_usb2_lpm_policy(struct usb_device *udev)
 		usb_set_usb2_hardware_lpm(udev, 1);
 	}
 }
-#endif
 
 static int hub_enable_device(struct usb_device *udev)
 {
@@ -4670,9 +4668,7 @@ hub_port_init(struct usb_hub *hub, struct usb_device *udev, int port1,
 	/* notify HCD that we have a device connected and addressed */
 	if (hcd->driver->update_device)
 		hcd->driver->update_device(hcd, udev);
-#ifndef CONFIG_VENDOR_SMARTISAN
 	hub_set_initial_usb2_lpm_policy(udev);
-#endif
 fail:
 	if (retval) {
 		hub_port_disable(hub, port1, 0);
